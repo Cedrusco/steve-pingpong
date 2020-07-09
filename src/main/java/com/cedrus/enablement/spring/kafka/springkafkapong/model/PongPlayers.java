0451f17e6407df45e4b26878c42b56dff547f4e6
Steve Kuo <NH43780@hertz.net>
2020-07-09 09:37:36 -0400

    modify key in Kstream to specify player

---
+++ src/main/java/com/cedrus/enablement/spring/kafka/springkafkapong/model/PongPlayers.java
@@ -0,0 +1,6 @@
+package com.cedrus.enablement.spring.kafka.springkafkapong.model;
+
+public enum PongPlayers {
+  PlayerA,
+  PlayerB,
+}
--- src/main/java/com/cedrus/enablement/spring/kafka/springkafkapong/stream/TopologyProvider.java
+++ src/main/java/com/cedrus/enablement/spring/kafka/springkafkapong/stream/TopologyProvider.java
@@ -3,6 +3,7 @@ package com.cedrus.enablement.spring.kafka.springkafkapong.stream;
 import com.cedrus.enablement.spring.kafka.springkafkapong.config.AppConfig;
 import com.cedrus.enablement.spring.kafka.springkafkapong.config.TopicConfig;
 import com.cedrus.enablement.spring.kafka.springkafkapong.model.PongBall;
+import com.cedrus.enablement.spring.kafka.springkafkapong.model.PongPlayers;
 import com.cedrus.enablement.spring.kafka.springkafkapong.model.PongTarget;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Random;
@@ -10,6 +11,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
@@ -37,45 +39,51 @@ public class TopologyProvider {
         this.objectMapper = objectMapper;
     }
 
-    public Topology getTopology(PongTarget pongTarget) {
-        final StreamsBuilder builder = new StreamsBuilder();
-        log.info("Stream builder initialized");
-        log.info(pongTarget.toString());
+        public Topology getTopology(PongTarget pongTarget) {
+            final StreamsBuilder builder = new StreamsBuilder();
+            log.info("Stream builder initialized");
+            log.info(pongTarget.toString());
 
-        final KStream<String, String> incomingStream = builder.stream(topicConfig.getTopicName(), Consumed.with(Serdes.String(), Serdes.String()));
+            final KStream<String, String> incomingStream = builder.stream(topicConfig.getTopicName(), Consumed.with(Serdes.String(), Serdes.String()));
 
-        final KStream<String, String> filteredStream = incomingStream.branch((key, value) -> {
-            PongBall pingPongBall = deserialize(value);
-            log.info("deserialize value: {}", pingPongBall);
-            return pingPongBall.getPongTarget().equals(pongTarget);
-        })[0];
+            final KStream<String, String> playerStream = incomingStream.map((key, value) -> {
+                log.info("incomingStream key before randomizer ={}", key);
+                return KeyValue.pair(randomKey(), value);
+            });
 
-        final KStream<String, String> loggedAndDelayedStream = filteredStream.transformValues(getLogsAndDelay());
+            final KStream<String, String> filteredStream = playerStream.branch((key, value) -> {
+                log.info("playerStream key after randomizer ={}", key);
+                PongBall pingPongBall = deserialize(value);
+                log.info("deserialize value: {}", pingPongBall);
+                return pingPongBall.getPongTarget().equals(pongTarget);
+            })[0];
 
-        loggedAndDelayedStream.to(topicConfig.getTopicName(), Produced.with(Serdes.String(), Serdes.String()));
+            final KStream<String, String> loggedAndDelayedStream = filteredStream.transformValues(getLogsAndDelay());
 
-        return builder.build();
-    }
+            loggedAndDelayedStream.to(topicConfig.getTopicName(), Produced.with(Serdes.String(), Serdes.String()));
 
-    private PongBall deserialize(String pingPongBallString) {
-        try {
-            return objectMapper.readValue(pingPongBallString, PongBall.class);
-        } catch (Exception e) {
-            log.debug("Deserialize error: {}", pingPongBallString);
-            throw new RuntimeException(e);
+            return builder.build();
         }
-    }
 
-    private String serialize(PongBall pingPongBall) {
-        try {
-            return objectMapper.writeValueAsString(pingPongBall);
-        } catch (Exception e) {
-            log.debug("Serialize error: {}", pingPongBall);
-            throw new RuntimeException(e);
+        private PongBall deserialize(String pingPongBallString) {
+            try {
+                return objectMapper.readValue(pingPongBallString, PongBall.class);
+            } catch (Exception e) {
+                log.debug("Deserialize error: {}", pingPongBallString);
+                throw new RuntimeException(e);
+            }
         }
-    }
 
-    private ValueTransformerSupplier<String, String> getLogsAndDelay() {
+        private String serialize(PongBall pingPongBall) {
+            try {
+                return objectMapper.writeValueAsString(pingPongBall);
+            } catch (Exception e) {
+                log.debug("Serialize error: {}", pingPongBall);
+                throw new RuntimeException(e);
+            }
+        }
+
+        private ValueTransformerSupplier<String, String> getLogsAndDelay() {
         return () -> new ValueTransformer<String, String>() {
             @Override
             public void init(ProcessorContext context) {}
@@ -107,4 +115,18 @@ public class TopologyProvider {
             public void close() {}
         };
     }
+
+        private String randomKey() {
+            int pick = new Random().nextInt(PongPlayers.values().length);
+
+            String randomPlayer = PongPlayers.values()[pick].name();
+
+            try {
+              log.info("random player ={}", randomPlayer);
+              return objectMapper.writeValueAsString(randomPlayer);
+            } catch (Exception e) {
+                log.debug("Serialize error: {}", randomPlayer);
+                throw new RuntimeException(e);
+            }
+        }
 }
